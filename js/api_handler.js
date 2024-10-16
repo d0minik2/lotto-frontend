191036962aec2145f1ad0f971643e4bd382ad99f
dominjk <dominik@salena.com.pl>
2024-10-16 15:05:19 +0200

    init

---
+++ js/api_handler.js
@@ -0,0 +1,145 @@
+function time_start() {
+    const date = new Date();
+    time_started = date.getTime()
+}
+
+function calculate_avg_time(years_passed) {
+    const date = new Date();
+    now = date.getTime()
+
+    time = now - time_started
+
+    // in milliseconds 
+    avg_time_per_year = time / years_passed
+    console.log(avg_time_per_year, time, time_started, now)
+    return Math.round(avg_time_per_year * 100) / 100
+}
+
+function update_time_html(avg) {
+    $(".time > i").text(avg)
+}
+
+
+$(window).bind('beforeunload', function () {
+    const postURL = apiURL + UID
+    fetch(postURL, { method: "DELETE" })
+    //  removing data from db when unloading
+});
+
+
+function update_html(data) {
+    $(".games-played").text(data.games_played)
+    $(".years-passed").text(data.years_passed)
+    $(".money-earned").text(data.money_earned)
+    $(".money-spent").text(data.money_spent)
+    $(".total-balance").text(data.total_balance)
+    $(".top-price-wins").text(data.top_price_wins)
+
+    if (data.top_price_wins > 0) {
+        WON = true
+    }
+
+    update_time_html(calculate_avg_time(data.years_passed))
+}
+
+
+async function init_simulation() {
+    // POST on api
+    const postURL = apiURL + UID
+
+    // remove previous session if exists
+    await fetch(postURL, { method: "DELETE" })
+
+    const simulationType = $("#lottery-type").val()
+
+    const body = {
+        "lottery_name": simulationType
+    }
+
+    const guess = get_guess()
+    if (!(guess === undefined)) {
+        body["guess"] = guess
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+
+    await fetch(postURL, options)
+        .then(response => {
+
+            return response.json();
+        })
+        .then(data => {
+            true_guess = data["guess"]
+            if (guess === undefined) {
+                set_guess(true_guess)
+            }
+
+            disable_guess()
+            // console.log(true_guess)
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        })
+}
+
+
+async function call_simulation() {
+    // GET on api
+    const postURL = apiURL + UID
+
+    // TODO add speed param
+
+    const options = {
+        method: "GET",
+    }
+
+    await fetch(postURL, options)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            update_html(data)
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        })
+}
+
+
+async function start_simulation() {
+    time_start()
+    // INIT
+    if (!paused) {
+        // TODO liczenie czasu
+        await init_simulation()
+    } else {
+        paused = false
+    }
+
+    while (!WON && !paused) {
+        await call_simulation()
+    }
+}
+
+function pause_simulation() {
+    paused = true
+}
+
+
+
+const apiURL = "http://dominik.chiptric.com/lottery/"
+let WON = false
+let paused = false
+let time_started
+
+function generate_uid() {
+    return self.crypto.randomUUID()
+}
+const UID = generate_uid()
+console.log(UID)
---
+++ js/guess.js
@@ -0,0 +1,125 @@
+const lotteries = {
+    "Lotto": {
+        name: "Lotto",
+        guess_table: [[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]]]
+    },
+    "EuroJackpot": {
+        name: "EuroJackpot",
+        guess_table: [[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]], [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]]
+    }
+
+}
+let guess_fields = []
+let checked_numbers = []
+
+// TODO wywalaj z opcji wybrane juz liczby dla kazdej sekcji
+
+function load_guess(lottery_name) {
+    const guess_table = lotteries[lottery_name].guess_table
+    guess_div = $(".guess")
+    guess_div.empty()
+    checked_numbers = []
+    guess_fields = []
+
+    guess_table.forEach(section => {
+        const section_div = $("<div>").addClass("guess-section")
+        let section_fields = []
+
+        section.forEach(field => {
+            // const input_field = $(`<input type="number" min="${field[0]}" max="${field[field.length - 1]}">`).addClass("guess-input")
+            const select_field = $(`<select>`).addClass("guess-input")
+
+            field.forEach(number => {
+                select_field.append($(`<option value="${number}">${number}</option>`))
+            })
+
+            section_div.append(select_field)
+            section_fields.push(select_field)
+        })
+
+        checked_numbers.push([])
+        guess_div.append(section_div)
+        guess_fields.push(section_fields)
+
+    })
+    console.log(guess_fields)
+}
+
+
+function get_guess() {
+    if (!$("#random-guess").is(":checked")) {
+        let guess = []
+        let correct = true
+
+        guess_fields.forEach(section => {
+            let section_guess = []
+            section.forEach(field => {
+                let n = parseInt(field.find(":selected").val())
+                if (!section_guess.includes(n)) {
+                    section_guess.push(n)
+                    guess.push(n)
+                } else {
+                    correct = false
+                    return
+                }
+
+            })
+        })
+        if (correct) {
+            return guess
+        }
+        alert("Liczby nie mogą się powtarzać w tej samej sekcji.")
+        return false
+    }
+
+}
+
+function disable_guess() {
+    guess_fields.forEach(section => {
+        section.forEach(field => {
+            field.prop('disabled', true)
+        })
+    })
+}
+
+function enable_guess() {
+    guess_fields.forEach(section => {
+        section.forEach(field => {
+            field.prop('disabled', false)
+        })
+    })
+}
+
+function set_guess(guess) {
+    for (let sect_i = 0; sect_i < guess_fields.length; sect_i++) {
+        for (let field_i = 0; field_i < guess_fields[sect_i].length; field_i++) {
+            guess_fields[sect_i][field_i].val(`${guess[sect_i][field_i]}`).change()
+        }
+    }
+}
+
+function random_guess_change() {
+    let checked = $("#random-guess").is(":checked")
+
+    if (checked) {
+        disable_guess()
+    } else {
+        enable_guess()
+    }
+}
+
+
+$(document).ready(function () {
+    load_guess($("#lottery-type").val())
+    random_guess_change()
+
+    $("#lottery-type").on("change", function () {
+        load_guess($("#lottery-type").val())
+        random_guess_change()
+    })
+
+    $("#random-guess").on("change", function () {
+        random_guess_change()
+    })
+
+})
